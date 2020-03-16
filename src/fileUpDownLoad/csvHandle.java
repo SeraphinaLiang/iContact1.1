1042021253f6c6c0583433aae7346ed7ac081648
SeraphinaLiang <1010814876@qq.com>
2020-03-16 11:35:54 +0800

    csvhandel

--- src/fileUpDownLoad/csvHandle.java
+++ src/fileUpDownLoad/csvHandle.java
@@ -1,12 +1,14 @@
 package fileUpDownLoad;
 
+import java.io.File;
+
 public class csvHandle {
 
 	public static void readCSV(String path) {
-		
+		File file=new File(path);
 	}
 	
 	public static void exportCSV(String dir) {
-		
+	//	File file = new File(dir + "/" + cp.getId() + ".csv");
 	}
 }
